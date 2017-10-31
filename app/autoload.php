f2491d32c449d9e3dc095b591a86d9baf0122766
Lukasz Tulikowski <Lukasz.Tulikowski@printed.com>
2017-10-31 22:21:30 +0000

    Update

--- app/autoload.php
+++ app/autoload.php
@@ -7,8 +7,5 @@ use Composer\Autoload\ClassLoader;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase {}
-class PHPUnit_Util_ErrorHandler extends \PHPUnit\Util\ErrorHandler {}
-class PHPUnit_Util_Test extends \PHPUnit\Util\Test {}
 
 return $loader;
